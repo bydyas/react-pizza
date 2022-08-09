3a5410b456bc03819437b8116f4a19f12c028fcb
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2022-08-09 18:24:35 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://62f273e3b1098f1508132820.mockapi.io/items')
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.log);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {data.map(({ id, ...pizza }) => (
+              <PizzaBlock key={id} {...pizza} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
